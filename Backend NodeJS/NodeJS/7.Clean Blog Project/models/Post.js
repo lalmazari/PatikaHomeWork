12ee660e92230d6f682eea8f10af03c7e0c7dcbb
lalmazari <lalmzr@icloud.com>
2022-06-24 22:24:12 +0300

    Part 3 Clean Blog

---
+++ Backend NodeJS/NodeJS/7.Clean Blog Project/models/Post.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+  title: String,
+  detail: String,
+  createdDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
+
